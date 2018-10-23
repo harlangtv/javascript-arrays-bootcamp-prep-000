959d17ba443f37ce24c7c09341110dcf51993291
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-23 19:30:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,6 @@ function removeElementFromBeginningOfArray(array){
 function destructivelyRemoveElementFromEndOfArray(array){
   array.pop()
 }
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1)
+}
