99da2585a3064a0e05efb2c0d13356182cc87bdf
Harlan Green-Taub <harlan.gt@gmail.com>
2018-10-23 19:15:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,6 +18,5 @@ function accessElementInArray(array, index){
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  array[shift]
-  return array
+  array.shift()
 }
